e007bd166e96400adc5bcc5fc5acd45bbed7fc26
centuser <centuser@centos7.example.com>
2018-06-15 17:48:08 +0900

    コントローラー

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,2 +1,10 @@
 class CompanyController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  # GET /books
+  # GET /books.json
+  def index
+    @companies = Company.all
+  end
+
 end
