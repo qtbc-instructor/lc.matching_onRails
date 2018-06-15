4b4b9d4932299d1a1dfcf9cb064da3dbe8b1f275
qtbc201804.frdm10@gmail.com <qtbc201804.frdm10@gmail.com>
2018-06-15 12:22:21 +0900

    user、status_master uniquekey 追加

--- db/migrate/20180614095351_create_users.rb
+++ db/migrate/20180614095351_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :tel
-      t.string :mail_address
+      t.string :tel, unique: true
+      t.string :mail_address, unique: true
       t.string :string
       t.string :pass
       t.string :string
