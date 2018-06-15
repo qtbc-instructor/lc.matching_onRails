ba14d894976d595ffb3f7c73d75dc01c193abce1
centuser <centuser@centos7.example.com>
2018-06-15 11:45:30 +0900

    skill_masterのskilltypeにuniquekey制約追加

--- db/migrate/20180614095530_create_skill_masters.rb
+++ db/migrate/20180614095530_create_skill_masters.rb
@@ -2,6 +2,7 @@ class CreateSkillMasters < ActiveRecord::Migration[5.0]
   def change
     create_table :skill_masters do |t|
       t.string :skilltype
+      t.index :skilltype , unique:true
 
       t.timestamps
     end
