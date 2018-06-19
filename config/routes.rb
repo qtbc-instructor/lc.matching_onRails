93f3a309c1a47ddecefde87adc88b61c8de69680
centuser <centuser@centos7.example.com>
2018-06-19 18:12:23 +0900

    19日作業分

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-  #resources :users
-
-  get 'users/view'
-
-  get 'login/index'
-  post 'login/auth'
-  get 'login/logout'
-  get '/company', controller: 'company', action: 'index'
-
-  #resources :lecturer
-
-  get 'lecturer/index'
   #LECTURER ROUTES
   get 'lecturer/:id' => 'lecturer#index'
 
