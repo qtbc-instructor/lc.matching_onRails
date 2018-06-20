a55e948a3e1f25ab548286b65be864a8c8bda025
FNDM36 <r-kawabata@froide.co.jp>
2018-06-20 10:22:06 +0900

    こんふり

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+
   root 'users#index' 
   #LECTURER ROUTES
   get 'lecturer/:id' => 'lecturer#index'
@@ -18,11 +18,5 @@ Rails.application.routes.draw do
   get 'login/logout'
   get '/company', controller: 'company', action: 'index'
   get 'lecturer/index'
-=======
-
-  #LECTURER ROUTES
-  get 'lecturer/:id' => 'lecturer#index'
-
->>>>>>> 93f3a309c1a47ddecefde87adc88b61c8de69680
-
+  
 end
