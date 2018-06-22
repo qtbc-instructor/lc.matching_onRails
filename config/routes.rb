71aa0595c1d862dd639cc58302018b4ae41d61e8
centuser <centuser@centos7.example.com>
2018-06-22 10:17:28 +0900

    models修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   post 'login/auth'
   get 'login/logout'
 
-  get 'lecturer/index'
 
   #resources :companies
   get '/company', controller: 'company', action: 'index'
@@ -32,7 +31,6 @@ Rails.application.routes.draw do
   get 'lecturer/index'
 
   #LECTURER ROUTES
-  get 'lecturer/:id' => 'lecturer#index'
   post 'lecturer/newfreeday' => 'lecturer#create_free'
   post 'lecturer/addskill' => 'lecturer#add_skill'
   post 'lecturer/deletefree' => 'lecturer#delete_free'
