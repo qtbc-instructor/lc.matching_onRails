c9595bab44bf301a3e3db2e5bd1ad75018daf382
centuser <centuser@centos7.example.com>
2018-06-25 19:01:02 +0900

    コントローラーの修正push不要

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -161,43 +161,5 @@ class CompanyController < ApplicationController
      flash[:notice] = "講師に申請できませんでした。"
      redirect_to :action => "search"
    end
-=======
-
-    logger.debug "=========================================="
-    # 評価更新
-    params.each do |key,value|
-      if key[0,8] == "statusID"
-        id = key[8,10].to_i
-        s = Status.find(id)
-        s.score = value
-        s.status_master_id = 5
-        s.save
-        s.errors.messages
-      end
-    end
-    logger.debug "=========================================="
-    # @a = params[:id]
-    # @u = Status.find(@a)
-    # @score = @u.update(score:5)
-    # redirect_to "/company", action: :update
-
-    # @getId = Status.find(params[:id])
-    # @getId.each do |state|
-    #   @status_id = state.id
-    #   @status_score = state.score
-    # end
-
-    # @status_score = Status.where(params[:id])
-    # @score = @status_score.update(score:5)
-    # respond_to do |score|
-    #   if @score.update(update)
-    #     score.html { redirect_to @score, notice: '評価されました。'}
-    #     score.json { render :search, status: :ok, location: @score}
-    #   else
-    #     score.html { render :search }
-    #     score.json { render json: @score.errors, status: :unprocessable_entity }
-    #   end
-    # end
-    redirect_to "/company", action: :update
- end
+  end
 end
