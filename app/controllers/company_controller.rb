725e7ac77dc1cdfd22e651b1da4673df1e11e56c
centuser <centuser@centos7.example.com>
2018-06-27 11:10:17 +0900

    カンパニー

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,11 +1,16 @@
 class CompanyController < ApplicationController
+
   def index
+
     @user = @usr
     @user_id = @user.id
-    @Applyings = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date').where("status_master_id = ?", 1).where("company_id = @user")
-    @Approval = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date').where("status_master_id = ?", 2).where("company_id = @user")
-    @Denial = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date').where("status_master_id = ?", 3).where("company_id = @user")
-    @Score = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date, statuses.score').where("status_master_id = ?", 4).where("company_id = @user")
+    @companies = User.joins(:company).select('users.id,companies.companyname,companies.id').find(@user_id)
+
+    # status_master_idによってわかれる
+    @Applyings = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date').where("status_master_id = ?", 1).where("company_id = ?", @companies.id)
+    @Approval = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date').where("status_master_id = ?", 2).where("company_id = ?", @companies.id)
+    @Denial = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date').where("status_master_id = ?", 3).where("company_id = ?", @companies.id)
+    @Score = Status.joins(:user, :skill_master).select('statuses.id, users.name, skill_masters.skilltype, statuses.date, statuses.score').where("status_master_id = ?", 4).where("company_id = ?", @companies.id)
   end
 
   def destroy
@@ -24,8 +29,8 @@ class CompanyController < ApplicationController
   end
 
   def update
+    # 評価更新の確認
     logger.debug "=========================================="
-    # 評価更新
     params.each do |key,value|
       if key[0,8] == "statusID"
         id = key[8,10].to_i
@@ -38,5 +43,76 @@ class CompanyController < ApplicationController
     end
     logger.debug "=========================================="
     redirect_to "/company", action: :update
+
+     @users_status = User.joins(:freeday, skill: {skill_master: :status})
+       .select('users.*,freedays.begin,freedays.end,skill_masters.skilltype,statuses.score')
+       .where( skills: { skill_master_id: session[:search_skill] })
+       .where( freedays: { begin: session[:search_freeday] })
+       .where( statuses: { user_id: @lecture_id })
+
+   end
+
+
+  def search
+    #Viewで検索条件表示に使用
+    @skill_masters = SkillMaster.all
+
+    session[:search_skill] = params[:skill]
+    session[:search_freeday] = params[:begin]
+
+
+    @free = Freeday.select('id,begin,end')
+    @free.each do |f|
+      @id = f.id
+      @begin = f.begin
+      @end = f.end
+    end
+
+    #検索実行
+      @users = User.joins(:freeday, skill: :skill_master)
+       .select('users.*,freedays.begin,freedays.end,skill_masters.skilltype')
+       .where( skills: { skill_master_id: session[:search_skill] } )
+       .where( freedays: { begin: session[:search_freeday] } )
+
+      @users.each do |u_score|
+        @lecture_id = u_score.id
+      end
+
+    @users_status = User.joins(:freeday, skill: {skill_master: :status})
+      .select('users.*,freedays.begin,freedays.end,skill_masters.skilltype,statuses.score')
+      .where( skills: { skill_master_id: session[:search_skill] })
+      .where( freedays: { begin: session[:search_freeday] })
+      .where( statuses: { user_id: @lecture_id })
+
+  end
+
+  def result
+    @search_userid = params[:id]
+
+    @users = User.joins(:freeday,:company,:skill)
+     .select('users.id,name,freedays.begin,companies.id,companyname,skills.skill_master_id')
+     .where(@search_userid)
+
+    @companies = User.joins(:company)
+      .select('users.id,companies.id')
+     .find(@usr.id)
+
+     @status = Status.new()
+     @status.user_id = @search_userid
+     @status.company_id = @companies.id
+     @status.skill_master_id = session[:search_skill]
+     @status.status_master_id = 1
+     @status.date = session[:search_freeday]
+     @status.score = 0
+     logger.debug "=======================-"
+    if @status.save
+     flash[:notice] = "講師に申請しました。"
+     redirect_to :action => "index"
+    else
+      logger.debug @status.errors.messages
+      logger.debug "=======================-"
+     flash[:notice] = "講師に申請できませんでした。"
+     redirect_to :action => "search"
+   end
   end
 end
